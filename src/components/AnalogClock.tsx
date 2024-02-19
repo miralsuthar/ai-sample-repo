39be5c6b317173e6e512c596074444d1ccf81ade
miralsuthar <miralsuthar2000@gmail.com>
2024-02-19 16:38:34 +0530

    fix: clock

--- src/components/AnalogClock.tsx
+++ src/components/AnalogClock.tsx
@@ -16,13 +16,14 @@ export const Clock = () => {
     };
   }, []);
 
+
   const seconds = time.getSeconds();
   const minutes = time.getMinutes();
   const hours = time.getHours();
 
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  const minsDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
-  const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+  const secondsDegrees = (seconds / 60) * 360;
+  const minsDegrees = (minutes / 60) * 360 + (seconds / 60) * 6;
+  const hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30;
 
   const [isClicked, setIsClicked] = useState(false);
   return (
--- src/components/DraggableClock.tsx
+++ src/components/DraggableClock.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import clockBg from '../assets/clockBg.png'
 import { Clock } from './AnalogClock'
 import { DndContext, useDroppable } from '@dnd-kit/core';
@@ -17,10 +17,33 @@ const Droppable = ({ id, children }: { id: string; children: React.ReactNode }) 
 
 export const DraggableClock = ({ shades200 }: { shades200: string }) => {
   const [parent, setParent] = useState(null);
+  const [time, setTime] = useState<string>("");
 
   const handleDragEnd = ({ over }: { over: any }) => {
     setParent(over ? over.id : null)
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString('en-IN', {
+        timeStyle: 'short',
+        hour12: true
+      }))
+    }, 60000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
+  useEffect(() => {
+    const currentTime = new Date().toLocaleTimeString('en-IN', {
+      timeStyle: 'short',
+      hour12: true
+    });
+
+    setTime(currentTime.split("p")[0]);
+  }, [])
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
@@ -30,7 +53,7 @@ export const DraggableClock = ({ shades200 }: { shades200: string }) => {
           background: `linear-gradient(180deg, rgba(255,255,255,0) 0%, ${shades200} 100%)`
         }} className='absolute top-0 left-0 h-full w-full bg-gradient-to-t opacity-60'></div>
         {!parent ? <Clock /> : <div className='h-[8.75rem] w-[8.75rem]'></div>}
-        <span className='text-[2rem] text-white z-10'>9:00</span>
+        <span className='text-[2rem] text-white z-10'>{time}</span>
         <div className='w-[8.75rem] h-[8.75rem] absolute left-2/4 bottom-[3.875rem] rounded-full border-dashed border border-white/30 -translate-x-2/4'>
           <Droppable id="droppable">
             {parent === "droppable" ? <Clock /> : <div className='h-full w-full'></div>}
